4bc5a72bd028acca26ff2822a3006b2b89c3df22
Nevil <nevil@tunerlabs.com>
2024-02-16 14:01:25 +0530

    updated response

--- src/routes/index.js
+++ src/routes/index.js
@@ -98,7 +98,7 @@ module.exports = (app) => {
 			// Detailed error response
 			const status = error.statusCode || 500
 			const responseCode = error.responseCode || 'SERVER_ERROR'
-			const message = error.message || 'Oops! Something went wrong on our end. Please try again later.'
+			const message = error.message || 'Oops! Something Went Wrong.'
 			const errorData = error.data || []
 
 			logger.info(message, { message: error })
@@ -119,7 +119,7 @@ module.exports = (app) => {
 			res.status(status).json(options)
 		} else {
 			// Limited info response
-			const errorMessage = 'Oops! Something went wrong on our end. Please try again later.'
+			const errorMessage = 'Oops! Something Went Wrong.'
 
 			logger.error('Server error!', { message: error.stack, triggerNotification: true })
 			console.error('Error occurred on the server:')

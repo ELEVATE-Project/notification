a9a29647d7aef2f5f2f87d1bf6046d7cea4f66ad
priyanka-TL <priyankapratheepan@gmail.com>
2025-04-11 12:54:58 +0530

    secure take from env

--- src/generics/helpers/email-notifications.js
+++ src/generics/helpers/email-notifications.js
@@ -123,7 +123,7 @@ async function sendEmail(params) {
 				const transporter = nodemailer.createTransport({
 					host: process.env.SMTP_HOST,
 					port: process.env.SMTP_PORT || 587,
-					secure: false,
+					secure: process.env.SMTP_SECURE || false,
 					auth: {
 						user: process.env.SMTP_USER,
 						pass: process.env.SMTP_PASS,
